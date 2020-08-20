77e3ad721f9f0e445ef6df807f7ff39834127de3
Henrique Pereira Borges <hpborges@gmail.com>
2020-08-20 09:11:12 -0300

    Add form tool and url skeleton
    Todo:
    	- Add view for add ingredient
    	- Add view for all ingredients
    	- Add view for manage ingredient

--- djangoproj/recipes/urls.py
+++ djangoproj/recipes/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('recipe/<int:recipe_id>', views.manage_recipe, name='manage recipe'),
+    path('recipe/new', views.create_recipe, name='create recipe'),
+    path('recipe/', views.list_recipe, name='all recipes'),
+    path('ingredient/<int:ingredient_id>', views.manage_ingredient, name='manage ingredient'),
+    path('ingredient/new', views.create_ingredient, name='create ingredient'),
+    path('ingredient/', views.list_ingredient, name='all ingredient')
 ]
--- djangoproj/recipes/views.py
+++ djangoproj/recipes/views.py
@@ -3,4 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse('Hello, world!')
+
+def list_recipe(request):
+    return HttpResponse('All recipes')
+
+def list_ingredient(request):
+    return HttpResponse('List all ingredients')
+
+def create_recipe(request):
+    return HttpResponse('You are creating a recipe!')
+
+def create_ingredient(request):
+    return HttpResponse('You are creating an ingredient!')
+
+def manage_recipe(request, recipe_id):
+    return HttpResponse('You are editing recipe {}'.format(recipe_id))
+
+def manage_ingredient(request, ingredient_id):
+    return HttpResponse('You are editing ingredient {}'.format(ingredient_id))
